0b42789b2a041bea007d009a17ceb0e6bdf54cc8
Jongwon Park <pjw970220@gmail.com>
2025-11-10 19:26:55 +0900

    Revert "feat: 로그인 레이아웃 구성"

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import Login from "./login/Login";
-import { Route, Routes } from "react-router-dom";
-import Register from "./register/Register";
 
 const Layout = () => {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </div>
-  );
+  return <div>Layout</div>;
 };
 
 export default Layout;
