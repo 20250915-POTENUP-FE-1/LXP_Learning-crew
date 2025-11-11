52fdaaf065ce9e71b77de237aba4e8ea5e9885c6
Jongwon Park <pjw970220@gmail.com>
2025-11-11 12:43:58 +0900

    edit: 주석 제거

--- src/hooks/service/useAuth.js
+++ src/hooks/service/useAuth.js
@@ -4,50 +4,6 @@ import postFetchLogout from "../../apis/auth/postFetchLogout";
 import getFetchAuthState from "../../apis/auth/getFetchAuthState";
 
 const useAuth = () => {
-  // const [isLoggedIn, setIsLoggedIn] = useState(() => {
-  //   const savedAuthState = localStorage.getItem("isLoggedIn");
-  //   return savedAuthState === "true";
-  // });
-
-  // const [user, setUser] = useState(() => {
-  //   const savedUser = localStorage.getItem("user");
-  //   return savedUser ? JSON.parse(savedUser) : null;
-  // });
-
-  // useEffect(() => {
-  //   const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-  //     if (currentUser) {
-  //       // 로그인 상태
-  //       setIsLoggedIn(true);
-  //       setUser(currentUser);
-  //       localStorage.setItem("isLoggedIn", "true");
-  //       localStorage.setItem(
-  //         "user",
-  //         JSON.stringify({
-  //           uid: currentUser.uid,
-  //           email: currentUser.email,
-  //           displayName: currentUser.displayName,
-  //         }),
-  //       );
-  //     } else {
-  //       // 로그아웃 상태
-  //       setIsLoggedIn(false);
-  //       setUser(null);
-  //       localStorage.setItem("isLoggedIn", "false");
-  //       localStorage.removeItem("user");
-  //     }
-  //   });
-
-  //   return () => unsubscribe();
-  // }, []);
-  // const user = useMemo(() => {
-  //   const unsubscribe = getFetchAuthState().then((userId) => {
-  //     return userId;
-  //   });
-
-  //   return unsubscribe;
-  // }, []);
-
   /**
    * 새로운 계정을 생성합니다.
    * @param {string} email 계정이 사용할 이메일
