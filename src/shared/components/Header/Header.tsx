50b41482383b4160080dc5cde853929ad2d3f2f6
Jongwon Park <pjw970220@gmail.com>
2025-12-04 11:08:00 +0900

    feat: header, footer 레이아웃

---
+++ src/shared/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import React from "react";
+import LogoImage from "../../../../public/logo.png";
+import Link from "next/link";
+import { ActionButton } from "../ActionButton";
+
+const Header = () => {
+  return (
+    <div className="flex h-16 w-full items-center justify-center">
+      <header className="flex w-full max-w-[1100px] items-center justify-between">
+        {/* Logo */}
+        <Link href="/">
+          <Image
+            className="h-10 w-[100px]"
+            src={LogoImage}
+            alt={"Learning Crew"}
+          />
+        </Link>
+
+        <div>
+          <Link href="/login">
+            <ActionButton variant="secondary" value={"로그인"} />
+          </Link>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
