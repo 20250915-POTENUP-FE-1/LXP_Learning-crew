53d8e4f15701132c677a6042477735186aa5c1a2
Jongwon Park <pjw970220@gmail.com>
2025-12-05 23:43:56 +0900

    fix: 경로 문제 수정

--- src/shared/constants/routes.ts
+++ src/shared/constants/routes.ts
@@ -1,7 +1,7 @@
 const APP_ROUTES = {
   MAIN: {
     DEFAULT: "/main",
-    LECTURE_DETAIL: "/main/", // /[lectureId]
+    LECTURE_DETAIL: "/main", // /[lectureId]
   },
   LOGIN: "/login",
   MY: "/my",
