54adf255666b41dfa643ff5f0ba91fddbe5af6e6
Jongwon Park <pjw970220@gmail.com>
2025-12-09 16:23:05 +0900

    edit: isFull 속성 추가

--- src/shared/components/ActionButton/ActionButton.tsx
+++ src/shared/components/ActionButton/ActionButton.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import type { ActionButtonProps } from "./ActionButton.type";
 import actionButtonStyle from "./ActionButton.style";
+import clsx from "clsx";
 
 const ActionButton = ({
   value,
+  isFull,
   variant,
   size,
   onClick,
-  ...rest
 }: ActionButtonProps) => {
   return (
     <button
-      className={actionButtonStyle({ variant, size })}
+      className={clsx(actionButtonStyle({ variant, size }), {
+        "flex-1": isFull,
+      })}
       onClick={onClick}
-      {...rest}
     >
       {value}
     </button>
--- src/shared/components/ActionButton/ActionButton.type.ts
+++ src/shared/components/ActionButton/ActionButton.type.ts
@@ -1,6 +1,11 @@
 import type { ButtonHTMLAttributes } from "react";
 
-type ActionButtonVariant = "primary" | "secondary" | "none";
+type ActionButtonVariant =
+  | "primary"
+  | "primaryBorder"
+  | "secondary"
+  | "secondaryBorder"
+  | "none";
 
 interface ActionButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   value: string;
@@ -8,6 +13,7 @@ interface ActionButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 
   variant?: ActionButtonVariant;
+  isFull?: boolean;
   size?: "medium" | "large";
 }
 
