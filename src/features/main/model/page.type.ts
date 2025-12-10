2fcaef5f59bba87c9a70d1d1917212f868938184
Jongwon Park <pjw970220@gmail.com>
2025-12-10 09:47:58 +0900

    edit: page.type.ts -> props.type.ts 이름 변경

--- src/features/main/model/page.type.ts
+++
@@ -1,9 +0,0 @@
-import { CourseCardProps } from "@/shared/components/CourseCard/CourseCard.type";
-import { ReactNode } from "react";
-
-interface CourseListProps {
-  name: string;
-  courses?: CourseCardProps[];
-}
-
-export type { CourseListProps };
