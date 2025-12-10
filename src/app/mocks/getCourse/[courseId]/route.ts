9c653c97370ab8107d0bda4b10f339b7816b26ee
Jongwon Park <pjw970220@gmail.com>
2025-12-10 09:46:13 +0900

    fix: promise 오류 해결

--- src/app/mocks/getCourse/[courseId]/route.ts
+++ src/app/mocks/getCourse/[courseId]/route.ts
@@ -2,12 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import MOCK_STORE from "../../store.mock";
 
 interface Params {
-  params: { courseId: string };
+  params: Promise<{ courseId: string }>;
 }
 
-export const GET = async ({ params }: Params) => {
+export const GET = async (request: NextRequest, { params }: Params) => {
+  const { courseId } = await params;
+
   const course = MOCK_STORE.courses.find(
-    async (course) => course.courseId === (await params.courseId),
+    async (course) => course.courseId === courseId,
   );
 
   return NextResponse.json({ course }, { status: 200 });
